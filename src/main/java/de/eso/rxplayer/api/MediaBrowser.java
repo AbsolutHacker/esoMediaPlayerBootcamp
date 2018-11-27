84e86363fa76e45ac8b719331586c723103632e7
veri4912 <extern.vetle.riekert@esolutions.de>
2018-11-27 12:27:25 +0100

    [non-functional] add documentation MediaBrowser Interface

--- src/main/java/de/eso/rxplayer/api/MediaBrowser.java
+++ src/main/java/de/eso/rxplayer/api/MediaBrowser.java
@@ -18,20 +18,52 @@ public interface MediaBrowser {
   }
   List<Album> getAlbums(EnumSet<Audio.Connection> searchScope);
 
+    /**
+     * [Comfort Function] <br/>
+     * Calls {@link searchAlbum} with the global searchScope - {@link globalSS}
+     * @param name of the album to search
+     * @return List of albums that were found
+     */
   default List<Album> searchAlbum(String name) {
     return searchAlbum(name, globalSS);
   }
+
+    /**
+     * Search for all occurrences of an album inside the supplied connections
+     * @param name of the album to search
+     * @param searchScope Connections which to search through
+     * @return List of albums that were found
+     */
   List<Album> searchAlbum(String name, EnumSet<Audio.Connection> searchScope);
+
+    /**
+     * [Comfort Function] <br/>
+     * Calls {@link searchTrack} with the global searchScope - {@link globalSS}
+     * @param name of the track to search
+     * @return List of tracks that were found
+     */
   default List<Track> searchTrack(String name) {
     return searchTrack(name, globalSS);
   }
+
+    /**
+     * Search for all occurrences of a track inside the supplied connections
+     * @param name of the track to search
+     * @param searchScope Connections which to search through
+     * @return List of tracks that were found
+     */
   List<Track> searchTrack(String name, EnumSet<Audio.Connection> searchScope);
 
-  void setSearchScope(EnumSet<Audio.Connection> sourcesInScope);
-
-  // source: any, context/scope: given album
+    /**
+     * Get all tracks of the supplied album
+     * @param album to list the tracks of
+     * @return List of Tracks from the supplied Album, Empty List if album null or empty
+     */
   List<Track> getAlbumTracks(Album album);
 
-  // source: radio
+    /**
+     * Get all globally available radioStations
+     * @return List of Stations, Empty List if no stations currently available
+     */
   List<Station> getStations();
 }
