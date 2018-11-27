b531a9f49e5a5d516102d2ac74fb7911e6e511ba
mwor <mwor@codemanufaktur.com>
2018-11-27 12:06:01 +0100

    First Service Impl

--- src/main/java/players/UsbPlayer.java
+++ src/main/java/players/UsbPlayer.java
@@ -1,6 +1,28 @@
 package players;
 
+import de.eso.rxplayer.Audio;
+import de.eso.rxplayer.Browser;
+import de.eso.rxplayer.EntertainmentService;
+import de.eso.rxplayer.Player;
+import io.reactivex.schedulers.Schedulers;
+
 public class UsbPlayer {
+
+    Player usbService;
+
+    Browser browserService;
+
+    Audio audioService;
+
+    UsbPlayer(){
+        EntertainmentService entertainmentService = new EntertainmentService(Schedulers.computation());
+        usbService = entertainmentService.getUsb();
+        browserService = entertainmentService.getBrowser();
+        audioService = entertainmentService.getAudio();
+    }
+
+
+
     public static void main(String[] args) {
 
     }
