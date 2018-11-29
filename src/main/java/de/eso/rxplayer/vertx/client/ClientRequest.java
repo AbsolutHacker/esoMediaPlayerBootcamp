1a4527e3ef03bccfe421d8db2bacb82490bad1bf
nise6080 <nicolas.seufert@esolutions.de>
2018-11-29 15:29:31 +0100

    minor, but breaking changes to ApiRequest (and ClientRequest)

--- src/main/java/de/eso/rxplayer/vertx/client/ClientRequest.java
+++ src/main/java/de/eso/rxplayer/vertx/client/ClientRequest.java
@@ -10,18 +10,17 @@ public class ClientRequest extends ApiRequest {
   public static final ApiRequest.Type DEFAULT_TYPE = ApiRequest.Type.QUERY;
 
   @SuppressWarnings("unchecked")
-  public ClientRequest(final int id, final Type type, final Map<String,?> params) {
+  public ClientRequest(final int id, final Type type, final String target, final Map<String,?> params) {
     this.id = id;
     this.request = type.toString();
+    this.target = target;
 
     // This cast should never fail because *everything* extends Object
     this.params = (Map<String,Object>) params;
   }
 
   public ClientRequest(final int id, final Type type, String singleMethodToInvoke) {
-    this(id, type, new HashMap<String,String>(){{
-      put("target", singleMethodToInvoke);
-    }});
+    this(id, type, singleMethodToInvoke, new HashMap<String,Object>());
   }
 
   public ClientRequest(final int id, String singleMethodToInvoke) {
