4480135e8dff738d33ddb7e6b0419dfde849631c
veri4912 <extern.vetle.riekert@esolutions.de>
2018-11-30 12:25:44 +0100

    forgotten last addation of MediaDisplay

---
+++ src/main/java/de/eso/rxplayer/ui/MediaBrowserDisplay.java
@@ -0,0 +1,58 @@
+package de.eso.rxplayer.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MediaBrowserDisplay extends JFrame {
+
+    Font font = new Font("SansSerif", Font.PLAIN, 40);
+
+    private static MediaBrowserDisplay mediaBrowserDisplay;
+
+    List<PlayerButton> buttons = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new MediaBrowserDisplay();
+    }
+
+    public MediaBrowserDisplay() {
+        mediaBrowserDisplay = this;
+        mediaBrowserDisplay.setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel mainGrid = new JPanel(new GridLayout(3,3));
+
+        buttons.add(new DisplayAlbumsButton());
+        buttons.add(new SearchAlbumButton());
+        buttons.add(new SearchTrackButton());
+        buttons.add(new AlbumTracksButton());
+        buttons.add(new SwitchColorButton());
+        buttons.add(new SwitchLanguageButton());
+
+        for (PlayerButton button : buttons) {
+            mainGrid.add(button);
+        }
+
+        add(mainGrid);
+
+        pack();
+    }
+
+    public void changeLanguage(Language language) {
+        for (PlayerButton button : buttons) {
+            button.changeLanguage(language);
+        }
+    }
+
+    public void changeColorTheme(ColorTheme colorTheme) {
+        for (PlayerButton button : buttons) {
+            button.setStyling(colorTheme);
+        }
+    }
+
+    public static MediaBrowserDisplay getMediaBrowserDisplay() {
+        return mediaBrowserDisplay;
+    }
+}
--- src/main/java/de/eso/rxplayer/ui/PlayerButtonImpl.java
+++ src/main/java/de/eso/rxplayer/ui/PlayerButtonImpl.java
@@ -1,13 +1,180 @@
 package de.eso.rxplayer.ui;
 
-import de.eso.rxplayer.api.ApiResponse;
 import de.eso.rxplayer.vertx.client.Launcher;
-import io.reactivex.Observable;
 
 import java.awt.event.ActionListener;
+import java.util.Random;
 
+class DisplayAlbumsButton extends  PlayerButton {
+
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "All Albums";
+                break;
+            case German:
+                buttonText = "Alle Alben";
+                break;
+            default:
+                buttonText = "All Albums";
+                break;
+        }
+        setText(buttonText);
+    }
+
+    @Override
+    public String getText() {
+        return buttonText;
+    }
+
+    @Override
+    public ActionListener[] getActionListeners() {
+        ActionListener[] actionListeners = new ActionListener[1];
+        actionListeners[0] = e -> {
+            System.out.println("DisplayAlbumsButton pressed");
+            Launcher.getClient().subscribe(client -> {
+//                Observable<ApiResponse> apiResponse$ = client.newRequest("/browse/albums");
+//                apiResponse$.subscribe(apiResponse -> {
+//
+//                });
+            });
+        };
+        return actionListeners;
+    }
+}
+
+class SearchAlbumButton extends PlayerButton {
+
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "Search Album";
+                break;
+            case German:
+                buttonText = "Album suchen";
+                break;
+            default:
+                buttonText = "Search Album";
+                break;
+        }
+        setText(buttonText);
+    }
+
+    @Override
+    public String getText() {
+        return buttonText;
+    }
+
+    @Override
+    public ActionListener[] getActionListeners() {
+        ActionListener[] actionListeners = new ActionListener[1];
+        actionListeners[0] = e -> {
+            System.out.println("SearchAlbumButton pressed");
+            Launcher.getClient().subscribe(client -> {
+//                Observable<ApiResponse> apiResponse$ = client.newRequest("/browse/album", albumName);
+//                apiResponse$.subscribe(apiResponse -> {
+//
+//                });
+            });
+        };
+        return actionListeners;
+    }
+}
+
+class SearchTrackButton extends PlayerButton {
+
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "Search Track";
+                break;
+            case German:
+                buttonText = "Titel suchen";
+                break;
+            default:
+                buttonText = "Search Track";
+                break;
+        }
+        setText(buttonText);
+    }
+
+    @Override
+    public String getText() {
+        return buttonText;
+    }
+
+    @Override
+    public ActionListener[] getActionListeners() {
+        ActionListener[] actionListeners = new ActionListener[1];
+        actionListeners[0] = e -> {
+            System.out.println("SearchTrackButton pressed");
+            Launcher.getClient().subscribe(client -> {
+//                Observable<ApiResponse> apiResponse$ = client.newRequest("/browse/track", trackName);
+//                apiResponse$.subscribe(apiResponse -> {
+//
+//                });
+            });
+        };
+        return actionListeners;
+    }
+}
+
+class AlbumTracksButton extends PlayerButton {
+
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "Album Tracks";
+                break;
+            case German:
+                buttonText = "Album Titel";
+                break;
+            default:
+                buttonText = "Album Tracks";
+                break;
+        }
+        setText(buttonText);
+    }
+
+    @Override
+    public String getText() {
+        return buttonText;
+    }
+
+    @Override
+    public ActionListener[] getActionListeners() {
+        ActionListener[] actionListeners = new ActionListener[1];
+        actionListeners[0] = e -> {
+            System.out.println("AlbumTracksButton pressed");
+            Launcher.getClient().subscribe(client -> {
+//                Observable<ApiResponse> apiResponse$ = client.newRequest("/browse/albumTracks", albumName);
+//                apiResponse$.subscribe(apiResponse -> {
+//
+//                });
+            });
+        };
+        return actionListeners;
+    }
+}
 
 class BackButton extends PlayerButton {
+
+    @Override
+    void changeLanguage(Language language) {
+        //This button does not change with different languages
+    }
 
     @Override
     public String getText() {
@@ -20,7 +187,10 @@ class BackButton extends PlayerButton {
         actionListeners[0] = e -> {
             System.out.println("BackButton pressed");
             Launcher.getClient().subscribe(client -> {
-                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/back");
+//                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/back");
+//                apiResponse$.subscribe(apiResponse -> {
+//
+//                });
             });
         };
         return actionListeners;
@@ -30,9 +200,27 @@ class BackButton extends PlayerButton {
 
 class StopButton extends PlayerButton {
 
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "STOP";
+                break;
+            case German:
+                buttonText = "STOP";
+                break;
+            default:
+                buttonText = "STOP";
+                break;
+        }
+        setText(buttonText);
+    }
+
     @Override
     public String getText() {
-        return "STOP";
+        return  buttonText;
     }
 
     @Override
@@ -41,7 +229,7 @@ class StopButton extends PlayerButton {
         actionListeners[0] = e -> {
             System.out.println("StopButton pressed");
             Launcher.getClient().subscribe(client -> {
-                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/stop");
+//                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/stop");
             });
         };
         return actionListeners;
@@ -49,42 +237,52 @@ class StopButton extends PlayerButton {
 }
 
 
-class PlayButton extends PlayerButton {
+class PlayPauseButton extends PlayerButton {
+
+    private boolean isPlayMode = true; //if button press equals to play or pause
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "PLAY";
+                break;
+            case German:
+                buttonText = "SPIELEN";
+                break;
+            default:
+                buttonText = "PLAY";
+        }
+        setText(buttonText);
+    }
 
     @Override
     public String getText() {
-        return "PLAY";
+        return buttonText;
     }
 
     @Override
     public ActionListener[] getActionListeners() {
         ActionListener[] actionListeners = new ActionListener[1];
         actionListeners[0] = e -> {
-            System.out.println("PlayButton pressed");
-            Launcher.getClient().subscribe(client -> {
-                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/play");
-            });
-        };
-        return actionListeners;
-    }
-}
 
+            if (isPlayMode) {
+                System.out.println("PlayButton pressed");
+                isPlayMode = false;
+                buttonText = "PLAY";
+                Launcher.getClient().subscribe(client -> {
+//                    Observable<ApiResponse> apiResponse$ = client.newRequest("/play/play");
+                });
 
-class PauseButton extends PlayerButton {
-
-    @Override
-    public String getText() {
-        return "PAUSE";
-    }
-
-    @Override
-    public ActionListener[] getActionListeners() {
-        ActionListener[] actionListeners = new ActionListener[1];
-        actionListeners[0] = e -> {
-            System.out.println("PauseButton pressed");
-            Launcher.getClient().subscribe(client -> {
-                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/pause");
-            });
+            } else {
+                System.out.println("PauseButton pressed");
+                isPlayMode = true;
+                buttonText = "PAUSE";
+                Launcher.getClient().subscribe(client -> {
+//                    Observable<ApiResponse> apiResponse$ = client.newRequest("/play/pause");
+                });
+            }
         };
         return actionListeners;
     }
@@ -92,6 +290,11 @@ class PauseButton extends PlayerButton {
 
 
 class ForwardButton extends PlayerButton {
+
+    @Override
+    void changeLanguage(Language language) {
+        //this button does not change with different languages
+    }
 
     @Override
     public String getText() {
@@ -104,9 +307,96 @@ class ForwardButton extends PlayerButton {
         actionListeners[0] = e -> {
             System.out.println("ForwardButton pressed");
             Launcher.getClient().subscribe(client -> {
-                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/forward");
+//                Observable<ApiResponse> apiResponse$ = client.newRequest("/play/forward");
             });
         };
         return actionListeners;
     }
 }
+
+
+class SwitchLanguageButton extends PlayerButton {
+
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "Switch Language";
+                break;
+            case German:
+                buttonText = "Sprache Ändern";
+                break;
+            default:
+                buttonText = "Switch Language";
+                break;
+        }
+        setText(buttonText);
+    }
+
+    @Override
+    public String getText() {
+        return buttonText;
+    }
+
+    @Override
+    public ActionListener[] getActionListeners() {
+        ActionListener[] actionListeners = new ActionListener[1];
+        actionListeners[0] = e -> {
+            System.out.println("SwitchLanguageButton pressed");
+            Language language;
+            if (getText().equals("Switch Language")) {
+                language = Language.German;
+            } else {
+                language = Language.English;
+            }
+            MediaBrowserDisplay.getMediaBrowserDisplay().changeLanguage(language);
+        };
+        return actionListeners;
+    }
+}
+
+
+class SwitchColorButton extends PlayerButton {
+
+    private String buttonText;
+
+    @Override
+    void changeLanguage(Language language) {
+        switch (language) {
+            case English:
+                buttonText = "Change Theme";
+                break;
+            case German:
+                buttonText = "Thema Ändern";
+                break;
+            default:
+                buttonText = "Change Theme";
+                break;
+        }
+        setText(buttonText);
+    }
+
+    @Override
+    public String getText() {
+        return buttonText;
+    }
+
+    @Override
+    public ActionListener[] getActionListeners() {
+        ActionListener[] actionListeners = new ActionListener[1];
+        actionListeners[0] = e -> {
+            System.out.println("SwitchColorThemeButton pressed");
+            ColorTheme colorTheme;
+            if (new Random().nextBoolean()) {
+                colorTheme = ColorTheme.ESO;
+            } else {
+                colorTheme = ColorTheme.BLUE;
+            }
+            MediaBrowserDisplay.getMediaBrowserDisplay().changeColorTheme(colorTheme);
+        };
+        return actionListeners;
+    }
+}
+
